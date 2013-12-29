9999987dc5336252204cbc55a800cb466d586f50
root <root@maleek.(none)>
2013-12-29 09:05:10 +0300

    added arrow navigation image to list-subjects

--- src/com/msingiapp/About.java
+++ src/com/msingiapp/About.java
@@ -2,14 +2,19 @@ package com.msingiapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class About extends Activity{
+public class About extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		View title = getWindow().findViewById(android.R.id.title);
+		View titleBar = (View) title.getParent();
+		titleBar.setBackgroundColor(getResources().getColor(
+				R.color.dark_green_color));
 		setContentView(R.layout.about);
 	}
-	
+
 }
